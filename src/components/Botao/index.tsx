d56936dd1af0be078d1b03ab5aa5b4835ae62e3e
Carlos Siqueira <csiqueira@eb.com>
2023-04-09 20:16:07 -0300

    first commit

---
+++ src/components/Botao/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import style from './Botao.module.scss';
+
+interface Props {
+    type?: "button" | "submit" | "reset" | undefined,
+    onClick?: () => void,
+    children?: React.ReactNode
+}
+
+function Botao({onClick, type, children}: Props) {
+    return(
+        <button 
+            className={style.botao} 
+            type={type}
+            onClick={onClick}
+        >                
+            {children}
+        </button>
+    );
+}
+
+export default Botao;
